fbf5979e47f826485cc503ad531e6831966b743c
Gabe <41751617+glstewart17@users.noreply.github.com>
2019-12-08 17:11:48 -0500

    Add signup

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/new/', views.PostCreate.as_view(), name='post_create'),
     path('<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete'),
     path('<slug:slug>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
     path('accounts/profile/', views.ProfileList.as_view(), name='profile'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 
 class PostList(generic.ListView):
@@ -44,3 +46,8 @@ class ProfileList(generic.ListView):
 
     # queryset = Post.objects.filter(status=1).order_by('-created_on')
     # template_name = 'profile.html'
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
